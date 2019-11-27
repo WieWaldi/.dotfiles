343ecd2c10e88e26932b9a46c6b3a46587f6a410
Waldemar Schroeer <lassmichinruhe@rz-amper.de>
2019-11-27 09:40:38 +0100

    RDP template moved to template.sh

--- template.sh
+++ template.sh
@@ -9,5 +9,25 @@
 # 塚 﨑 晴 﨓  
 # ﱮ          
 #    
+#  
 
+# /bin/pkill compton
+/bin/xfreerdp --ignore-certificate -g 2400x1400 -d domain.com -u username -p password $1 &
+# ~/.local/bin/compton &
 
+function ThisFunction() {
+    # Do something useful
+    echo $1
+}
+
+casetype=$1
+case ${casetype} in
+    affe)
+        echo affe
+        ;;
+    *)
+        echo nicht affe
+        ;;
+esac
+
+exit 0 
