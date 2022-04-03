df228d83a7e13aa05074865a2c36d9270f259622
Waldemar Schroeer <w@ldemar.de>
2022-04-03 11:42:29 +0200

    Add skeleton files to .vim directory.

---
+++ .vim/templates/skeleton.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# +----------------------------------------------------------------------------+
+# | Name of the Script                                                         |
+# +----------------------------------------------------------------------------+
+# | Copyright © 2019 Waldemar Schroeer                                         |
+# |                  waldemar.schroeer(at)rz-amper.de                          |
+# +----------------------------------------------------------------------------+
+
+# Some glyphs I have often in use.
+# 塚 﨑 晴 﨓  
+# ﱮ          
+#    
+#                    
+#  
+
+ThisFunction() {
+    # Do something useful
+    echo $1
+}
+
+casetype=$1
+case ${casetype} in
+    affe)
+        echo affe
+        ;;
+    *)
+        echo nicht affe
+        ;;
+esac
+
+exit 0 
