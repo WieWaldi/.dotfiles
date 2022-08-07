e888ceea58fe24d66db1c52e657cfe0140640b10
Waldemar Schroeer <w@ldemar.de>
2022-08-07 19:06:09 +0200

    Minor changes.

--- .local/bin/tmux-start.sh
+++ .local/bin/tmux-start.sh
@@ -24,7 +24,7 @@ if [[ "$sessionexist" = "" ]]; then
 
     if [[ "$session" = "Login" ]]; then
         tmux send-keys -t $session:Master 'clear' C-m
-        tmux send-keys -t $session:Master '.local/share/fancy-motd/motd.sh' C-m
+        # tmux send-keys -t $session:Master '.local/share/fancy-motd/motd.sh' C-m
         tmux send-keys -t $session:Master '.local/bin/colortest-pac-man-3.sh' C-m
     fi
     if [[ "$session" = "ScratchPad1" ]]; then
