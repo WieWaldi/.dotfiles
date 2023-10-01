cfabeee97aef21acd9ad51612ebc15bba47c293c
Waldemar Schroeer <w@ldemar.de>
2023-10-01 21:22:40 +0200

    tmux-start.sh Minor changes on the way how tmux session gets build.

--- .local/bin/tmux-start.sh
+++ .local/bin/tmux-start.sh
@@ -15,27 +15,48 @@ fi
 session=$1
 sessionexist=$(tmux list-session | grep $session)
 
-if [[ "$sessionexist" = "" ]]; then
-    tmux new-session -d -s $session
-    tmux rename-window -t $session:1 'Master'
-    tmux new-window -t $session:2 -n 'Slave'
-    tmux new-window -t $session:3 -n 'Scratch'
-    tmux new-window -t $session:4 -n 'etc'
 
-    if [[ "$session" = "Login" ]]; then
-        tmux send-keys -t $session:Master 'clear' C-m
-        # tmux send-keys -t $session:Master '.local/share/fancy-motd/motd.sh' C-m
-        tmux send-keys -t $session:Master '.local/bin/colortest-pac-man-3.sh' C-m
-    fi
-    if [[ "$session" = "ScratchPad1" ]]; then
-        tmux send-keys -t $session:Master 'clear' C-m
-        tmux send-keys -t $session:Master 'figlet -f small RZ-Amper' C-m
-    fi
-    if [[ "$session" = "ScratchPad2" ]]; then
-        tmux send-keys -t $session:Master 'clear' C-m
-        tmux send-keys -t $session:Master 'bpytop' C-m
-    fi
+if [[ "$sessionexist" = "" ]]; then
+    case "${session}" in
+        Login)
+        tmux new-session -d -s $session
+        tmux rename-window -t $session:1 ' Master'
+        tmux new-window -t $session:2 -n ' Slave'
+        tmux new-window -t $session:3 -n ' Scratch'
+        tmux new-window -t $session:4 -n ' etc'
+        tmux new-window -t $session:5 -n ' System'
+        tmux new-window -t $session:6 -n ' Monitoring'
+        tmux new-window -t $session:7 -n ' Notes'
+        tmux new-window -t $session:8 -n ' Code 1'
+        tmux new-window -t $session:9 -n ' Code 2'
+        tmux send-keys -t $session:1 'clear' C-m
+        tmux send-keys -t $session:1 '.local/share/fancy-motd/motd.sh' C-m
+        tmux send-keys -t $session:1 '.local/bin/colortest-pac-man-3.sh' C-m
+        ;;
+    
+    ScratchPad1)
+        tmux new-session -d -s $session
+        tmux rename-window -t $session:1 ' Master'
+        tmux new-window -t $session:2 -n ' Slave'
+        tmux send-keys -t $session:1 'clear' C-m
+        tmux send-keys -t $session:1 'figlet -f small RZ-Amper' C-m
+        ;;
+    
+    ScratchPad2)
+        tmux new-session -d -s $session
+        tmux rename-window -t $session:1 ' Monitoring'
+        tmux new-window -t $session:2 -n ' System'
+        tmux send-keys -t $session:1 'clear' C-m
+        tmux send-keys -t $session:1 'bpytop' C-m
+        ;;
+    
+    *)
+        tmux new-session -d -s $session
+        tmux rename-window -t $session:1 ' Master'
+        tmux new-window -t $session:2 -n ' Slave'
+        ;;
+    esac
 fi
 
-tmux attach-session -t $session:Master
+tmux attach-session -t $session:1
 exit 0 
