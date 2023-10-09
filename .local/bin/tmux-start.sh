6494587669ff58dd9911f53766ee0e5fc5799167
Waldemar Schroeer <w@ldemar.de>
2023-10-09 23:44:29 +0200

    tmux-start.sh - Update glyphs and window names.

--- .local/bin/tmux-start.sh
+++ .local/bin/tmux-start.sh
@@ -22,13 +22,13 @@ if [[ "$sessionexist" = "" ]]; then
         tmux new-session -d -s $session
         tmux rename-window -t $session:1 ' Master'
         tmux new-window -t $session:2 -n ' Slave'
-        tmux new-window -t $session:3 -n ' Scratch'
-        tmux new-window -t $session:4 -n ' etc'
-        tmux new-window -t $session:5 -n ' System'
+        tmux new-window -t $session:3 -n '󰢹 Remote'
+        tmux new-window -t $session:4 -n ' etc'
+        tmux new-window -t $session:5 -n '󱄕 System'
         tmux new-window -t $session:6 -n ' Monitoring'
         tmux new-window -t $session:7 -n ' Notes'
-        tmux new-window -t $session:8 -n ' Code 1'
-        tmux new-window -t $session:9 -n ' Code 2'
+        tmux new-window -t $session:8 -n ' Code 1'
+        tmux new-window -t $session:9 -n ' Code 2'
         tmux send-keys -t $session:1 'clear' C-m
         tmux send-keys -t $session:1 '.local/share/fancy-motd/motd.sh' C-m
         tmux send-keys -t $session:1 '.local/bin/colortest-pac-man-3.sh' C-m
